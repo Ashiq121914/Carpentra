2fcce204f51ba7ce8c92f2471151908baba90263
GaziAshiq <ashiq121914@gmail.com>
2022-11-08 20:44:50 +0600

    details services page added

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ServicesCard from "./ServicesCard";
 
 const Services = () => {
-  return <div></div>;
+  const [services, setServices] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/services")
+      .then((res) => res.json())
+      .then((data) => setServices(data));
+  });
+  return (
+    <div className="grid lg:grid-cols-3 grid-cols-1 gap-8 lg:gap-x-11 container w-3/4 mx-auto mt-10">
+      {services.map((service) => (
+        <ServicesCard key={service._id} service={service}></ServicesCard>
+      ))}
+    </div>
+  );
 };
 
 export default Services;
---
+++ src/Pages/Services/ServicesCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ServicesCard = ({ service }) => {
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure>
+        <img src={service.img} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">
+          {service.title}
+          <div className="badge badge-secondary">NEW</div>
+        </h2>
+        <p>{service.description.slice(0, 100)}...</p>
+        <div className="card-actions justify-between mt-4">
+          <div className="badge badge-outline">${service.price}</div>
+          <div className="badge badge-outline">{service.rating}</div>
+        </div>
+        <div className="text-center">
+          <Link to={`/serviceDetails:${service._id}`}>
+            <div className="btn btn-outline-ghost">Details</div>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServicesCard;
